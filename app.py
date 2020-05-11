787dafbf08838e0a9bfee7dda95763668f63ff30
laurence100795 <laurence100795@gmail.com>
2020-05-11 17:50:22 +0000

    pages created with full navigation completed

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ app = Flask(__name__)
 def get_homepage():
     return render_template("index.html")
 
+@app.route('/get_recipes') # Sets the link for addrecipes.html to be used with other pages
+def get_recipes():
+    return render_template("recipe.html")
+
+@app.route('/get_addrecipes') # Sets the link for recipes.html to be used with other pages
+def get_addrecipes():
+    return render_template("addrecipe.html")
+
+@app.route('/get_amendrecipes') # Sets the link for amend recipes.html to be used with other pages
+def get_amendrecipes():
+    return render_template("amendrecipe.html")
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
