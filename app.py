b759befa320442a297c61fcc0b459904a5676b3b
laurence100795 <laurence100795@gmail.com>
2020-05-26 16:36:48 +0000

    Final Commit project ready for heroku

--- app.py
+++ app.py
@@ -20,18 +20,18 @@ def get_homepage():
 
 
 
-@app.route('/get_recipes') # Sets the link for addrecipes.html to be used with other pages
+@app.route('/get_recipes') # Sets the link for addrecipes.html to be used with other pages as well pass varuables to the page from mongo
 def get_recipes():
     return render_template("recipe.html", recipe_lists=mongo.db.recipe_lists.find())
 
 
 
-@app.route('/get_addrecipes') # Sets the link for recipes.html to be used with other pages
+@app.route('/get_addrecipes') # Sets the link for recipes.html to be used with other pages as well pass varuables to the page from mongo
 def get_addrecipes():
     return render_template("addrecipe.html", add_recipe=mongo.db.recipe_lists.find(), cook_time=mongo.db.cook_time.find(),
     difficulty=mongo.db.difficulty.find(), diets=mongo.db.diet.find(), serves=mongo.db.serves.find(), prep_time=mongo.db.prep_time.find())
 
-@app.route('/insert_recipe', methods=['POST'])
+@app.route('/insert_recipe', methods=['POST']) # insert new data to MongoDB
 def insert_recipe():
     addrecipe =  mongo.db.recipe_lists
     addrecipe.insert_one(request.form.to_dict())
@@ -39,13 +39,13 @@ def insert_recipe():
 
 
 
-@app.route('/get_amendrecipes') # Sets the link for amend recipes.html to be used with other pages
+@app.route('/get_amendrecipes') # Sets the link for amend recipes.html to be used with other pages as well pass varuables to the page from mongo
 def get_amendrecipes():
     return render_template("amendrecipe.html", recipe_lists=mongo.db.recipe_lists.find())
 
 
 
-@app.route('/get_editrecipes/<recipe_id>') # Sets the link for recipes.html to be used with other pages
+@app.route('/get_editrecipes/<recipe_id>') # Sets the link for recipes.html to be used with other pages as well pass varuables to the page from mongo
 def get_editrecipes(recipe_id):
     recipe =  mongo.db.recipe_lists.find_one({"_id": ObjectId(recipe_id)})
     return render_template('editrecipe.html', recipe=recipe, add_recipe=mongo.db.recipe_lists.find(), cook_time=mongo.db.cook_time.find(),
@@ -53,7 +53,7 @@ def get_editrecipes(recipe_id):
 
 
 
-@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+@app.route('/update_recipe/<recipe_id>', methods=["POST"]) # Updates existing variables in MongoDB linked to this app 
 def update_recipe(recipe_id):
     recipe = mongo.db.recipe_lists
     recipe.update( {'_id': ObjectId(recipe_id)},
@@ -73,7 +73,7 @@ def update_recipe(recipe_id):
 
 
 
-@app.route('/delete_recipe/<recipe_id>')
+@app.route('/delete_recipe/<recipe_id>') # Deletes variables that exist in MongoDB
 def delete_recipe(recipe_id):
     mongo.db.recipe_lists.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_amendrecipes'))
